3a9cdee11c832b39b323aafff73f4904acbef10c
Zhu-yt <13798337794@163.com>
2026-04-14 16:10:41 +0800

    feat: add suffix renaming feature, and refactor the code to split operations into independent functions

--- main.cpp
+++ main.cpp
@@ -2,42 +2,49 @@
 #include <windows.h>
 #include <string>
 
-int main(int argc, char* argv[]){//argc: number of argument; argv: argument content
-    using namespace std;
-    
-//add a prefix to rename
-    if(argc < 2){
-        cout << "usage: ./renamer.exe <prefix>" << endl;
-        return 1;
+using namespace std;
+
+void showHelp(){
+    cout << "========================================" << endl;
+    cout << "   batch file renaming tool" << endl;
+    cout << "========================================" << endl;
+    cout << "usage:" << endl;
+    cout << "  --prefix <text>    add prefix" << endl;
+    cout << "  --suffix <text>    add suffix" << endl;
+    cout << "  --dry-run          preview mode" << endl;
+    cout << "  --help, -h         show help" << endl;
+    cout << endl;
+    cout << "examples:" << endl;
+    cout << "  ./renamer.exe --prefix backup_           # add prefix" << endl;
+    cout << "  ./renamer.exe --suffix _backup           # add suffix" << endl;
+    cout << "  ./renamer.exe --prefix backup_ --dry-run # preview mode" << endl;
+    cout << "========================================" << endl;
+}
+
+string addPrefix(const string& filename, const string& prefix){
+    return prefix + filename;
+}
+
+string addSuffix(const string& filename, const string& suffix){
+    size_t dotPos = filename.find_last_of('.');
+
+    if(dotPos != string::npos){
+        string namepart = filename.substr(0,dotPos);
+        string extpart = filename.substr(dotPos);
+        return namepart + suffix + extpart;
     }
 
-    bool dryRun = false;
-    string prefix = argv[argc-1];
+    return filename + suffix;
+}
 
-    for(int i = 1;i < argc;i++){
-        if(string(argv[i]) == "--dry-run"){
-            dryRun = true;
-        }else{
-            prefix = argv[i];
-        }
-    }
-
-    if(prefix.empty()){
-        cout << "please specify the prefix" << endl;
-        return 1;
-    }
-
-    if(dryRun){
-        cout << "[preview] will add prefix: " << prefix << endl;
-    }
-
+void processFiles(const string& operation, const string& text, bool dryRun){
     WIN32_FIND_DATAA findData;
 
     //typedef void* HANDLE
     HANDLE hFind = FindFirstFileA(".//*", &findData);
     if(hFind == INVALID_HANDLE_VALUE){
         cout << "failed to open folder" << endl;
-        return 1;
+        return;
     }
 
     //on the basis of filtering out "." & "..", add folder judgment
@@ -67,7 +74,15 @@ int main(int argc, char* argv[]){//argc: number of argument; argv: argument cont
             continue;
         }
 
-        string new_name = prefix + filename;
+        string new_name;
+
+        if(operation == "prefix"){
+            new_name = addPrefix(filename, text);
+        }
+        if(operation == "suffix"){
+            new_name = addSuffix(filename, text);
+        }
+
         string old_path = ".\\" + filename;
         string new_path = ".\\" + new_name;
 
@@ -87,6 +102,52 @@ int main(int argc, char* argv[]){//argc: number of argument; argv: argument cont
     }while(FindNextFileA(hFind,&findData));
 
     FindClose(hFind);
+}
+
+int main(int argc, char* argv[]){//argc: number of argument; argv: argument content
+    
+    if(argc < 3){
+        showHelp();
+        return 1;
+    }
+
+    string operation;
+    string text;
+    bool dryRun = false;
+
+    for(int i = 1;i < argc;i++){
+        string arg = argv[i];
+        
+        if(arg == "--prefix"){
+            operation = "prefix";
+
+            if(i + 1 < argc){
+                text = argv[++i];
+            }
+
+        }else if(arg == "--suffix"){
+            operation = "suffix";
+
+            if(i + 1 < argc){
+                text = argv[++i];
+            }
+
+        }else if(arg == "--dry-run"){
+            dryRun = true;
+
+        }else if(arg == "--help" || arg == "-h"){
+            showHelp();
+            return 0;
+        }
+    }
+
+    if(operation.empty() || text.empty()){
+        cout << "Error: please specify an operation and its text" << endl;
+        showHelp();
+        return 1;
+    }
+
+    processFiles(operation, text, dryRun);
 
     return 0;
 }
